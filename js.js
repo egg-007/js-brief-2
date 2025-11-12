6798a5a69836b5d8dcc486880477b154e795a871
achraf abdelhakim essaloua <abdalhakimgo@gmail.com>
2025-11-12 21:11:34 +0100

    save button and add and remove

--- js.js
+++ js.js
@@ -13,6 +13,20 @@ document.getElementById('bar4'),
 document.getElementById('bar5')
 ]
 
+let schoolindex = 0;
+let jobindex = 0;
+
+const Education = {
+    school : [],
+    degree : [],
+    startDate : [],
+    endDate : [],
+    company : [],
+    job : [],
+    startJobDate : [],
+    endJobDate : [],
+};
+
 let currentIndex = 0 ;
 // for next page    
 function nextPage(test) {
@@ -51,7 +65,7 @@ function setupNextButton() {
     const nextbtn = document.getElementById('nextbtn');
     const Previousbtn = document.getElementById('Previousbtn');
     nextbtn.addEventListener('click', () => {
-        nextPage(true);
+        nextPage(true); //validation
     });
     Previousbtn.addEventListener('click', () => {
         PreviousPage(true);
@@ -98,27 +112,119 @@ inputval.addEventListener('keypress', (e) => {
     }
 })
 
+
 const div = document.getElementById('addskills')
 
-const btnclicked = div.querySelectorAll('button')
 
-btnclicked.forEach(button => {
-    button.addEventListener('click',(e) =>{
+div.addEventListener('click', (e) => {
+    const clicked = e.target.closest('button');
+    if(clicked){
         e.preventDefault();
-        if(button.id == "btn") {
-           document.getElementById('addskills').remove();
-           console.log("in1")
-        }
-        else{
-            let nid = Number(button.id) - 100;
-            let elem = document.getElementById(nid)
-            if(elem)
-                elem.remove();
-           console.log("in2")
-        }
-    })
+        clicked.parentElement.remove()
+    }
 })
 
+
+let softId = 1;
+
+const inputsoftval = document.getElementById('soft_skill')
+let addsoft = document.getElementById('soft_skills');
+const softContent = document.getElementById('cpid');
+const softButton = document.getElementById('cbtn');
+
+inputsoftval.addEventListener('keypress', (e) => { 
+    const inp = inputsoftval.value.trim()
+    const soft = document.getElementById('soft_skills_content').cloneNode(true)
+    if(e.key == 'Enter' && inp != ""){
+        e.preventDefault()
+        if(softId === 1)
+        {
+            document.getElementById('soft_skills_content').classList.remove('hiddenn');
+            softContent.textContent = inp;
+            softContent.id = (softId + 1000);
+            softButton.id = (softId + 100);
+            console.log("add")
+        }
+        else
+        {
+            soft.id = softId;
+            console.log("soft.id")
+            console.log(soft.id)
+            soft.classList.remove('hiddenn');
+            softContent.textContent = inp;
+            softContent.id = (softId + 1000);
+            softButton.id = (softId + 100);
+            addsoft.appendChild(soft)
+            console.log("add2")
+        }
+        softId++;
+
+        inputsoftval.value = ""
+    }
+})
+
+
+const softDiv = document.getElementById('soft_skills')
+
+
+softDiv.addEventListener('click', (e) => {
+    const clicked = e.target.closest('button');
+    if(clicked){
+        e.preventDefault();
+        clicked.parentElement.remove()
+    }
+})
+
+// Education
+const school = document.getElementById('school')
+const degree = document.getElementById('degree')
+const dateS = document.getElementById('dateS')
+const dateN = document.getElementById('dateN')
+const saveshool = document.getElementById('savebtn')
+const savejob = document.getElementById('savejobbtn')
+
+saveshool.addEventListener('click', (e) =>{
+    e.preventDefault();
+    Education.school[schoolindex] = school.value;
+    Education.startDate[schoolindex] = dateS.value;
+    Education.endDate[schoolindex] = dateN.value;
+    if(degree.value == "")
+        Education.degree[schoolindex] = "no degree";
+    else
+    Education.degree[schoolindex] = degree.value;
+    schoolindex++;
+    school.value = ""
+    dateS.value = ""
+    dateN.value = ""
+    degree.value = ""
+
+    const t = document.getElementById('schoolpart').id + schoolindex;
+    console.log(t);
+
+    console.log(Education);
+})
+
+// job
+const company = document.getElementById('company')
+const job = document.getElementById('job')
+const dateS_job = document.getElementById('dateS_job')
+const dateN_job = document.getElementById('dateN_job')
+
+savejob.addEventListener('click', (e) =>{
+    e.preventDefault();
+    Education.company[schoolindex] = company.value;
+    Education.job[schoolindex] = job.value;
+    Education.startJobDate[schoolindex] = dateS_job.value;
+    Education.endJobDate[schoolindex] = dateN_job.value;
+    jobindex++;
+    school.value = ""
+    dateS.value = ""
+    dateN.value = ""
+    degree.value = ""
+
+    const t = document.getElementById('schoolpart').id + schoolindex;
+
+})
 
 
 
